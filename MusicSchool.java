189b6982d11a05a1567ad422c5683d30cec4cd7c
tonm <michael.ton@colorado.edu>
2011-10-02 19:55:18 -0600

    created MusicSchool class per activity diagram. note that Classroom in  expects an array of Students. should Classroom create Students or MusicSchool?

--- MusicSchool.java
+++ MusicSchool.java
@@ -1,12 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class MusicSchool {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		System.out.println("Welcome to the Music School");
+		
+		Classroom paddedWalls = new Classroom(null); // CLASSROOM EXPECTS LIST OF STUDENTS!
+		SongDB songData = new SongDB("songs.txt");
+		Instructor teacher = new Instructor("Glenn Holland", paddedWalls);
 
+		
+		boolean contSim = true;
+		
+		do {
+			System.out.println("How many weeks should the simulation run?: ");
+			int duration = getStdInput();
+			
+			Clock timer = Clock.getInstance();
+			timer.start(duration);
+			
+			System.out.println("Would you like to repeat the simulation? (y/n): ");
+			char repeat = (char) System.in.read();
+			if(repeat == 'n') {
+				contSim = false;
+			}
+		} while(contSim);
+		
+		
+		
+		
+		
 	}
-
+	
+	public static int getStdInput() {
+		int duration = 0;
+		try {
+			BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+			duration = Integer.parseInt(is.readLine());
+		} catch (NumberFormatException ex) {
+			System.err.println("Invalid number. You fail at life.");
+		} catch (IOException e) {
+			System.err.println("Input failure.  You failer.");
+		}
+		return duration;
+	}
 }
